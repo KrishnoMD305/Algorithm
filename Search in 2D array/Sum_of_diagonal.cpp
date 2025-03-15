13545b508b63e839d4dcedcf734a2ada3af40b5a
Krishna Mondal <krishnomd60@gmail.com>
2025-03-15 10:39:37 +0600

    Update Sum_of_diagonal.cpp

--- Search in 2D array/Sum_of_diagonal.cpp
+++ Search in 2D array/Sum_of_diagonal.cpp
@@ -1,7 +1,19 @@
+// Square matrix
+
+//Principle diagonal
 int sum = 0;
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
             if(i==j){
+                sum += mat[i][j];
+            }
+        }
+    }
+
+// Sub diagonal
+for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(j==row-i-1){
                 sum += mat[i][j];
             }
         }
