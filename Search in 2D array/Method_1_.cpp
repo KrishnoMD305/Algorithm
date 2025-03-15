d2c999a8a3fd96ce94f0f0ad0449b5ddcdb87754
Krishna Mondal <krishnomd60@gmail.com>
2025-03-15 10:06:59 +0600

    Create Method_1_.cpp

---
+++ Search in 2D array/Method_1_.cpp
@@ -0,0 +1,8 @@
+for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(mat[i][j] == target){
+                cout<<i<<" "<<j<<endl;
+                
+            }
+        }
+    }
